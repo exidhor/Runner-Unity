36fd1c467ecadbc689e69893a663b504d4932a46
exidhor <jipay.detiege@gmail.com>
2016-02-19 05:30:08 +0100

    ajout du projet

---
+++ Assets/Assets/Scripts/GroundGeneration.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundGeneration : MonoBehaviour {
+
+	public Transform ground1;
+	public Transform ground2;
+
+	public Transform level1;
+	public Transform level2;
+
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void Update()
+	{
+		if (player.position.z - 275 > ground1.position.z) {
+			Vector3 movement = new Vector3 (0, 0, 1000);
+			level1.position += movement;
+		}
+		if (player.position.z - 275 > ground2.position.z) {
+			Vector3 movement = new Vector3 (0, 0, 1000);
+			level2.position += movement;
+		}
+	}
+}
---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speedX;
+	public float speedY;
+	public Text speed;
+	public Text youLoose;
+	public Text distanceText;
+	public int distance;
+
+	public bool isSpacePressed;
+
+	private Rigidbody rigidBody;
+	private float offset;
+
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody> ();
+		youLoose.text = "";
+		setSpeedText ();
+
+		offset = -rigidBody.position.z; 
+		setDistanceText ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		setSpeedText ();
+		setDistanceText ();
+	}
+
+	void FixedUpdate(){
+		//deplacement
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal*speedX, 0, moveVertical*speedY);
+
+		rigidBody.AddForce(movement*Time.deltaTime);
+
+		// test de fin de jeu
+		if (rigidBody.position.y < -5) {
+			youLoose.text = "You Lose N00b !";
+		}
+	}
+
+	void setSpeedText()
+	{
+		speed.text = "Speed : " + (int)(rigidBody.velocity.z);
+	}
+
+	void setDistanceText()
+	{
+		distance = (int)(rigidBody.position.z + offset);
+		distanceText.text = "Distance : " + distance;
+
+	}
+		
+}
